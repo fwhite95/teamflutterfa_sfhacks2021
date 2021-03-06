eb27195fe277298aee930df3b90f73f47874f396
white <white.freddie95@gmail.com>
2021-03-05 22:27:56 -0800

    merging

---
+++ lib/src/services/place_service.dart
@@ -0,0 +1,93 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart';
+import 'package:teamflutterfa_sfhacks2021/src/util/config.dart';
+
+class Place {
+  String streetNumber;
+  String street;
+  String city;
+  String zipCode;
+  String lat;
+  String lng;
+
+  Place({
+    this.streetNumber,
+    this.street,
+    this.city,
+    this.zipCode,
+    this.lat,
+    this.lng,
+  });
+
+  @override
+  String toString() {
+    return 'Place(lat: $lat, lng: $lng)';
+  }
+}
+
+class Suggestion {
+  final String placeId;
+  final String description;
+
+  Suggestion(this.placeId, this.description);
+
+  @override
+  String toString() {
+    return 'Suggestion(description: $description, placeId: $placeId)';
+  }
+}
+
+class PlaceApiProvider {
+  final client = Client();
+
+  PlaceApiProvider(this.sessionToken);
+
+  final sessionToken;
+
+  Future<List<Suggestion>> fetchSuggestions(String input, String lang) async {
+    final request =
+        'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$input&key=$googleApiKey&sessiontoken=$sessionToken';
+    final response = await client.get(request);
+
+    if (response.statusCode == 200) {
+      final result = json.decode(response.body);
+      if (result['status'] == 'OK') {
+        return result['predictions']
+            .map<Suggestion>((p) => Suggestion(p['place_id'], p['description']))
+            .toList();
+      }
+      if (result['status'] == 'ZERO_RESULTS') {
+        return [];
+      }
+      throw Exception(result['error_message']);
+    } else {
+      throw Exception('Failed to fetch suggestion');
+    }
+  }
+
+//get more details about the place from the placeId.
+//Use this to get lat lang?
+  Future<Place> getPlaceDetailFromId(String placeId) async {
+    final request =
+        'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&fields=geometry/location&key=$googleApiKey&sessiontoken=$sessionToken';
+    final response = await client.get(request);
+
+    if (response.statusCode == 200) {
+      final result = json.decode(response.body);
+      if (result['status'] == 'OK') {
+        print('Result: $result');
+        final place = Place();
+        
+        place.lat = result['result']['geometry']['location']['lat'].toString();
+        place.lng = result['result']['geometry']['location']['lng'].toString();
+        
+        return place;
+      }
+      throw Exception(result['error_message']);
+    }else {
+      throw Exception('Failed to fetch suggestion');
+    }
+  }
+}
