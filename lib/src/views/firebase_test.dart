a8da473a5af7330bb9ece8f1beca7e748c8bf66e
white <white.freddie95@gmail.com>
2021-03-07 07:31:18 -0800

    added config for firebase

---
+++ lib/src/views/firebase_test.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class FirebaseTest extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _FirebaseTestState();
+}
+
+class _FirebaseTestState extends State<FirebaseTest> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Test'),
+      ),
+    );
+  }
+
+}
